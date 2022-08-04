669d39130ecd3e104d63a936a9263447797b6b03
kgsws <kgsws@seznam.cz>
2022-08-04 19:35:02 +0200

    oops, duplicate code

--- code/main.c
+++ code/main.c
@@ -68,17 +68,5 @@ static const hook_t hooks[] __attribute__((used,section(".hooks"),aligned(4))) =
 	{0x00015A28, DATA_HOOK | HOOK_IMPORT, (uint32_t)&states},
 	{0x0001C3EC, DATA_HOOK | HOOK_IMPORT, (uint32_t)&mobjinfo},
 	{0x00012D90, DATA_HOOK | HOOK_IMPORT, (uint32_t)&weaponinfo},
-
-	// invert 'run key' logic (auto run)
-	{0x0001FBC5, CODE_HOOK | HOOK_UINT8, 0x01},
-	// allow invalid switch textures
-	{0x0003025B, CODE_HOOK | HOOK_CALL_DOOM, 0x000346F0},
-	{0x0003026A, CODE_HOOK | HOOK_CALL_DOOM, 0x000346F0},
-	// fix 'OUCH' face; this intentionaly fixes only 'OUCH' caused by enemies
-	{0x0003A079, CODE_HOOK | HOOK_UINT8, 0xD7},
-	{0x0003A081, CODE_HOOK | HOOK_UINT8, 0xFF},
-	{0x0003A085, CODE_HOOK | HOOK_JMP_DOOM, 0x0003A136},
-	{0x0003A137, CODE_HOOK | HOOK_UINT8, 8},
-	{0x0003A134, CODE_HOOK | HOOK_UINT8, 0xEB},
 };
 
